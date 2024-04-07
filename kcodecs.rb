c9ebbe9e813dc29ba6327dd0829bfd39b2029b2e
Tavis Palmer <tavis@tavispalmer.com>
2024-04-06 22:12:20 -0400

    Added kcodecs.rb

---
+++ kcodecs.rb
@@ -0,0 +1,38 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class Kcodecs < Formula
+  desc "Provide a collection of methods to manipulate strings using various encodings"
+  homepage "https://community.kde.org/Frameworks"
+  url "https://download.kde.org/stable/frameworks/6.0/kcodecs-6.0.0.tar.xz"
+  sha256 "ceba44dcdc251d443bbd6cb77d1219d876af8cc2ae4710472aace78122395abe"
+  license all_of: ["LGPL-2.0-only", "LGPL-3.0-only"]
+
+  depends_on "cmake" => :build
+  depends_on "doxygen" => :build
+  depends_on "extra-cmake-modules" => :build
+  depends_on "gperf" => :build
+  depends_on "qt"
+
+  def install
+    args = ["-DBUILD_TESTING=OFF",
+            "-DBUILD_QCH=ON"]
+
+    system "cmake", "-S", ".", "-B", "build", *std_cmake_args, *args
+    system "cmake", "--build", "build"
+    system "cmake", "--install", "build"
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test kcodecs`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
